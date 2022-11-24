df282412b21c17e551ffd2695536707f94bf9cf2
dminischetti <dminischetti@wsgc.com>
2022-11-23 21:58:15 -0800

    Initial commit with note editor

--- notes/main.js
+++ notes/main.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
     })
 
     win.loadFile('index.html')
@@ -16,7 +17,6 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
-
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
